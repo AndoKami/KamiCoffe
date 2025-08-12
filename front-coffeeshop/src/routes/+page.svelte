c6c869bce0bece2652ba96299f028b5df8418346
AndoKami <hei.ando.1007@gmail.com>
2025-08-12 23:09:05 +0300

    initial commit : ajout des fichiers local

---
+++ front-coffeeshop/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<div class="container" role="button" tabindex="0" on:click={() => console.log('Container clicked')} on:keydown={(e) => { if (e.key === 'Enter' || e.key === ' ') console.log('Container clicked'); }}>
+  <div class="top"></div>
+  <div class="bottom"></div>
+  <div class="center">
+    <h2>Please Sign In</h2>
+    <input type="email" placeholder="email" aria-label="Email address" />
+    <input type="password" placeholder="password" aria-label="Password" />
+    <h2>{' '}</h2> <!-- Remplace &nbsp; par un espace ou texte si nécessaire -->
+  </div>
+</div>
+
+<style>
+</style>
